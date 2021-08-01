bbdf3f75f668064c9dc004288f66c1c0dab3329f
Ingvar-V <ingvarvolkov@gmail.com>
2021-08-01 21:02:46 +0300

    Add files via upload

---
+++ 03_12/script.js
@@ -0,0 +1,16 @@
+import Car from "./Car.js";
+
+const myNewCar = new Car(
+  "Skoda Karoq",
+  2022,
+  "white",
+  "gas",
+  "four-wheeled drive",
+  "automatic",
+  "on",
+  true,
+  true
+);
+
+console.log("My new car option:", myNewCar);
+console.log("Mynew car model:", myNewCar.model);
